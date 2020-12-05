4474b2e9d5f0497205470e1570a0e4bcb810bb3b
Raine Revere <raine@cybersemics.org>
2020-12-05 12:31:07 -0700

    Upgrade to Typescript v4 (not working).

--- dist/index.js
+++ dist/index.js
@@ -1,19 +1,45 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.load = void 0;
 const converter_1 = require("typedoc/dist/lib/converter");
 const comment_1 = require("typedoc/dist/lib/converter/factories/comment");
 const abstract_1 = require("typedoc/dist/lib/models/reflections/abstract");
+const ts = require('typescript');
+function getModuleComment(text) {
+    const hasModuleComment = text.slice(0, 3) === '/**';
+    if (!hasModuleComment)
+        return 0;
+    const endCommentIndex = text.indexOf('*/');
+    const comment = text.slice(3, endCommentIndex).trim();
+    return comment;
+    // const parsedComment = comment.split('\n')
+    //   .map(line => line.replace(/^\s+\*+\s+/i, ''))
+    //   .join('\n')
+    // return pase
+}
 function load({ application }) {
+    /** Subscribe on EVENT_CREATE_DECLARATIN. */
     application.converter.on(converter_1.Converter.EVENT_CREATE_DECLARATION, (context, reflection, node) => {
-        reflection.kind = reflection.kind === abstract_1.ReflectionKind.Module ? abstract_1.ReflectionKind.ExternalModule : reflection.kind;
-        if (!node) {
+        if (!reflection.comment)
             return;
+        if (reflection.getFullName().includes('action-creators/index.ts')) {
+            const { text } = node.getSourceFile();
+            const moduleComment = getModuleComment(text);
+            reflection.comment = comment_1.parseComment(moduleComment);
         }
+        /** Re-define all Modules as External. */
+        if (reflection.kindOf(abstract_1.ReflectionKind.Module)) {
+            reflection.setFlag(abstract_1.ReflectionFlag.External);
+            // reflection.kind = reflection.kind === ReflectionKind.EternalModule
+        }
+        if (!node)
+            return;
+        /** Get comment from current node, which declaration is being created. */
         const rawComment = comment_1.getRawComment(node);
-        if (!rawComment) {
+        if (!rawComment)
             return;
-        }
-        reflection.comment = !reflection.comment ? comment_1.parseComment(rawComment) : reflection.comment;
+        /** If comment was added (e.g. for function, constants etc) - do nothing, otherwise (for modules without @packageDocumentation tag, which comment property is empty) add parsed comment. */
+        reflection.comment = comment_1.parseComment(rawComment);
     });
 }
 exports.load = load;
--- index.ts
+++ index.ts
@@ -1,23 +1,47 @@
 import {PluginHost} from 'typedoc/dist/lib/utils'
 import { Converter, Context } from 'typedoc/dist/lib/converter'
 import { getRawComment, parseComment } from "typedoc/dist/lib/converter/factories/comment"
-import { ReflectionKind } from 'typedoc/dist/lib/models/reflections/abstract'
+import { ReflectionFlag, ReflectionKind } from 'typedoc/dist/lib/models/reflections/abstract'
 import { DeclarationReflection } from 'typedoc'
+const ts = require('typescript')
+
+
+function getModuleComment(text) {
+  const hasModuleComment = text.slice(0, 3) === '/**'
+  if (!hasModuleComment) return 0
+  const endCommentIndex = text.indexOf('*/')
+  const comment = text.slice(3, endCommentIndex).trim()
+  return comment
+  // const parsedComment = comment.split('\n')
+  //   .map(line => line.replace(/^\s+\*+\s+/i, ''))
+  //   .join('\n')
+
+  // return pase
+}
 
 export function load({application}: PluginHost) {
   /** Subscribe on EVENT_CREATE_DECLARATIN. */
   application.converter.on(Converter.EVENT_CREATE_DECLARATION, (context: Context, reflection: DeclarationReflection, node) => {
-    /** Re-define all Modules as External. */
-    reflection.kind = reflection.kind === ReflectionKind.Module ? ReflectionKind.ExternalModule : reflection.kind
-    if (!node) {
-      return
+
+    if (!reflection.comment) return
+
+    if (reflection.getFullName().includes('action-creators/index.ts')) {
+      const { text}  = node.getSourceFile()
+      const moduleComment = getModuleComment(text)
+      reflection.comment = parseComment(moduleComment)
     }
+
+    /** Re-define all Modules as External. */
+    if (reflection.kindOf(ReflectionKind.Module)) {
+      reflection.setFlag(ReflectionFlag.External)
+      // reflection.kind = reflection.kind === ReflectionKind.EternalModule
+    }
+    if (!node) return
+
     /** Get comment from current node, which declaration is being created. */
     const rawComment = getRawComment(node)
-    if (!rawComment) {
-      return
-    }
+    if (!rawComment) return 
     /** If comment was added (e.g. for function, constants etc) - do nothing, otherwise (for modules without @packageDocumentation tag, which comment property is empty) add parsed comment. */
-    reflection.comment = !reflection.comment ? parseComment(rawComment) : reflection.comment
+    reflection.comment = parseComment(rawComment)
   })
 }
